414c9190ec59820febcb66b651be54c1f04b202f
claudiebeaulieu <beaulieu@ucsc.edu>
2026-09-23 19:41:09 +0000

    Deploying to gh-pages from @ claudiebeaulieu/claudiebeaulieu.github.io@e9ac128a69af7e21c5dbee477a4f9e95a19e61d3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -63,6 +63,9 @@ ninja.data = [{
           section: "News",},{id: "news-the-beta-version-of-the-temperature-trends-explorer-dashboard-is-now-live-check-it-out-here",
           title: 'The beta version of the Temperature Trends Explorer dashboard is now live, check...',
           description: "",
+          section: "News",},{id: "news-our-paper-on-regional-warming-accelerations-is-now-published-in-nature-communications-check-it-out-here-and-here-is-the-link-to-the-ucsc-press-release",
+          title: 'Our paper on regional warming accelerations is now published in Nature Communications! Check...',
+          description: "",
           section: "News",},{
         id: 'social-email',
         title: 'email',
